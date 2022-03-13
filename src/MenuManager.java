febab658d2ddd8f0772095051bed04447d467c29
SungChoon3 <kuhang7@gmail.com>
2022-03-14 00:25:38 +0900

    removed hello.java and added MenuManger.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add xx");
+		System.out.println("2. Delete xx");
+		System.out.println("3. Edit xx");
+		System.out.println("4. View");
+		System.out.println("5. Exit");
+	}
+
+}
