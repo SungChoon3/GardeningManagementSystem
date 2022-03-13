9fd8cf03a2bae07043800294c8ed28bc0081f374
SungChoon3 <kuhang7@gmail.com>
2022-03-14 00:36:12 +0900

    Rename Project name to GardeningManagementSystem

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,9 +2,9 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add xx");
-		System.out.println("2. Delete xx");
-		System.out.println("3. Edit xx");
+		System.out.println("1. Add Plant");
+		System.out.println("2. Delete Plant");
+		System.out.println("3. Edit Plant");
 		System.out.println("4. View");
 		System.out.println("5. Exit");
 	}
